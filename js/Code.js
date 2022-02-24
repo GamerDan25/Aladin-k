f6c4768defcf72b20f7889a1970b3a373c6c2dfa
GamerDan25 <86494699+GamerDan25@users.noreply.github.com>
2022-02-24 15:41:27 -0600

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,29 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+
+    fill("white")
+    textSize(15)
+    text(" N U T C F I N O ", 700,160)
+    fill("lightblue")
+    text("Hint: perform  a particular task !!", 700,180)
+
+    fill("white")
+    textSize(15)
+    text("A B A T A S E D", 100,200)
+    fill("lightblue")
+    text("Hint: Store all information!!", 100,220)
+
+    
+
+    
+
+}
